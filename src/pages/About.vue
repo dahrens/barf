d7e15b39d1bd99a9c3919d08e431b6adbe97613f
Dennis Ahrens <dennis.ahrens@mailbox.org>
2017-10-02 18:01:26 +0200

    Content for about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="container">
-    About
+  <div class="about">
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            B.A.R.F. calculator and planning app
+          </h1>
+          <h2 class="subtitle">
+            <strong>B</strong>are
+            <strong>A</strong>nd
+            <strong>R</strong>aw
+            <strong>F</strong>ood
+          </h2>
+        </div>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>B.A.R.F.</strong> by <a href="https://github.com/dahrens">Dennis Ahrens</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+          <p>
+            <a class="icon" href="mailto:dennis.ahrens@mailbox.org">
+              <fa pack="fas" name="envelope" />
+            </a>
+            <a class="icon" href="https://github.com/dahrens/barf">
+              <fa pack="fab" name="github" />
+            </a>
+            <a class="icon" href="https://stackoverflow.com/users/5992118/dahrens">
+              <fa pack="fab" name="stack-overflow" />
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
