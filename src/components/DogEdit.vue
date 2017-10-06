a24650e4c844ef40f731421cf91f3162b9d6b475
Dennis Ahrens <dennis.ahrens@mailbox.org>
2017-10-06 07:48:35 +0200

    Remove generic form compnents.

--- src/components/DogEdit.vue
+++ src/components/DogEdit.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <stringInput
-      label="Name"
-      :value="instance.name"
-      @change="(val) => instance.name = val"
-      :validate="validateName"
-      validateError="Name must be not empty and unique"
-      iconLeft="address-card">
-    </stringInput>
+    <div class="panel-block">
+      <div class="field-label is-normal">
+        <label class="label">Name</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control has-icons-left">
+            <input type="text"
+              class="input"
+              :class="{'is-danger': !validateName(), 'is-success': validateName()}"
+              v-model="dog.name"
+              placeholder="Dogs Name">
+            <span class="icon is-left">
+              <fa pack="fas" name="address-card" />
+            </span>
+          </p>
+          <p v-if="!validateName()" class="help is-danger">Dogs name must be not empty and unique</p>
+        </div>
+      </div>
+    </div>
 
     <div class="panel-block">
       <div class="field-label is-normal">
@@ -21,17 +33,6 @@
         </div>
       </div>
     </div>
-
-    <numberInput
-      label="Weight"
-      :min="1000" :max="150000" :step="500"
-      :value="instance.currentWeight"
-      @change="(val) => instance.currentWeight = val"
-      :validate="validateWeight"
-      helpText="The weight your dog currently has in gram"
-      validateError="Weight in gram must be a positive integer value."
-      iconLeft="tachometer-alt">
-    </numberInput>
 
     <div class="panel-block">
       <div class="field-label is-normal">
@@ -77,37 +78,36 @@
         </div>
       </div>
     </div>
-    <booleanInput
-      label="Castrated"
-      :value="instance.castrated"
-      @change="(val) => instance.castrated = val"
-      helpText="Is you dog castrated? Click me to switch">
-    </booleanInput>
+
+    <div class="panel-block">
+      <div class="field-label is-normal">
+        <label class="label">Castrated</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <a class="button"
+            v-on:click="dog.castrated = !dog.castrated">
+            <span class="icon" :class="{'has-text-success': dog.castrated, 'has-text-danger': !dog.castrated}">
+              <fa v-if="dog.castrated" pack="fas" name="check-circle" />
+              <fa v-else pack="fas" name="times-circle" />
+            </span>
+          </a>
+          <p class="help is-info">Is you dog castrated? Click me to switch</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import stringInput from '@/components/include/EditString'
-import numberInput from '@/components/include/EditNumber'
-import booleanInput from '@/components/include/EditBoolean'
 import Datepicker from 'vue-bulma-datepicker'
 
 export default {
   name: 'dogEdit',
   props: ['dog'],
-  data () {
-    return {
-      instance: null
-    }
-  },
   components: {
-    stringInput,
-    numberInput,
-    booleanInput,
     Datepicker
-  },
-  created () {
-    this.instance = this.dog
   },
   computed: {
     activities () {
@@ -125,25 +125,22 @@ export default {
   methods: {
     validate () {
       let results = [
-        this.validateName(this.instance.name),
-        this.validateWeight(this.instance.weight)
+        this.validateName()
       ]
       return results.every(r => r)
     },
-    validateName (name) {
+    validateName () {
+      let name = this.dog.name
       if (name === '') {
         return false
       }
       let dogs = this.$store.state.dogs
-      if (this.instance.hasOwnProperty('id')) {
-        dogs = dogs.filter(d => d.id !== this.instance.id)
+      if (this.dog.hasOwnProperty('id')) {
+        dogs = dogs.filter(d => d.id !== this.dog.id)
       }
       if (dogs.some(d => d.name === name)) {
         return false
       }
-      return true
-    },
-    validateWeight () {
       return true
     }
   }
--- src/components/PlanMetaEdit.vue
+++ src/components/PlanMetaEdit.vue
@@ -1,72 +1,58 @@
 <template>
   <div>
-    <numberInputField
-      label="Weight"
-      :min="1000" :max="150000" :step="500"
-      :value="instance.plan.idealWeight"
-      @change="(val) => instance.plan.idealWeight = val"
-      :validate="validateWeight"
-      helpText="This is the ideal weight based on which we calculate the amount per day"
-      validateError="Weight in gram must be a positive integer value."
-      iconLeft="tachometer-alt">
-    </numberInputField>
+    <div class="panel-block">
+      <div class="field-label is-normal">
+        <label class="label">Weight</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control has-icons-left">
+            <input type="number"
+              :min="1000" :max="150000" :step="500"
+              class="input"
+              :class="{'is-danger': !validateWeight(), 'is-success': validateWeight()}"
+              v-model="dog.currentWeight">
+            <span class="icon is-left">
+              <fa pack="fas" name="tachometer-alt" />
+            </span>
+          </p>
+          <p v-if="!validateWeight()" class="help is-danger">Weight in gram must be a positive integer value.</p>
+          <p v-if="validateWeight()" class="help is-info">The weight in gram your dog has right now.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel-block">
+      <div class="field-label is-normal">
+        <label class="label">Calculated</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <a class="button"
+            v-on:click="dog.plan.calculated = !dog.plan.calculated">
+            <span class="icon" :class="{'has-text-success': dog.plan.calculated, 'has-text-danger': !dog.plan.calculated}">
+              <fa v-if="dog.plan.calculated" pack="fas" name="check-circle" />
+              <fa v-else pack="fas" name="times-circle" />
+            </span>
+          </a>
+          <p class="help is-info">Should B.A.R.F. calculate the daily portion for you?</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import stringInputField from '@/components/include/EditString'
-import numberInputField from '@/components/include/EditNumber'
-import Datepicker from 'vue-bulma-datepicker'
-
 export default {
   name: 'planMetaEdit',
   props: ['dog'],
-  data () {
-    return {
-      instance: null
-    }
-  },
-  components: {
-    stringInputField,
-    numberInputField,
-    Datepicker
-  },
-  created () {
-    this.instance = this.dog
-  },
-  computed: {
-    activities () {
-      let options = []
-      for (let activity in this.$store.state.activities) {
-        let value = this.$store.state.activities[activity]
-        options.push({
-          activity,
-          value
-        })
-      }
-      return options
-    }
-  },
   methods: {
     validate () {
       let results = [
-        this.validateName(this.instance.name),
-        this.validateWeight(this.instance.weight)
+        this.validateWeight()
       ]
       return results.every(r => r)
-    },
-    validateName (name) {
-      if (name === '') {
-        return false
-      }
-      let dogs = this.$store.state.dogs
-      if (this.instance.hasOwnProperty('id')) {
-        dogs = dogs.filter(d => d.id !== this.instance.id)
-      }
-      if (dogs.some(d => d.name === name)) {
-        return false
-      }
-      return true
     },
     validateWeight () {
       return true
